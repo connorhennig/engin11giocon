6313bc89a2f0452edbe3d6756ea0f294a5e7ff1a
connorhennig <124406934+connorhennig@users.noreply.github.com>
2023-03-24 14:56:41 -0700

    Update radiationSensorLab.py

--- radiationSensorLab.py
+++ radiationSensorLab.py
@@ -1,11 +1,26 @@
 import RPi.GPIO as GPIO
 import datetime
 import time
+import csv
+import sys
 
 channel = 16
 
 count = 0
 mincount = 0
+
+run_time = int(sys.argv[1])
+
+interval = float(sys.argv[2])
+
+if (len(sys.argv) > 3):
+    file_name = sys.argv[3]
+    
+print(file_name)
+file = open(file_name,"w", newline='')
+writer = csv.writer(file)
+meta_data = ["Time",CPM]
+writer.writerow(meta_data)
 
 def my_callback(channel):
     global count 
@@ -14,14 +29,19 @@ def my_callback(channel):
     mincount += 1
     print('Radiation detected at ' + str(datetime.datetime.now())) 
 
-while True:
+start_time = time.time()
+
+while ((time.time() - start_time) <= run_time):
     current_time = time.time()
-    while (( time.time() - current_time ) <= 60):
+    while (( time.time() - current_time ) <= interval):
         try:
             GPIO.setmode(GPIO.BCM)
             GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-            GPIO.add_event_detect(channel, GPIO.FALLING, callback= my_callback, bouncetime=200)
+            GPIO.add_event_detect(channel, GPIO.FALLING, callback= my_callback, bouncetime=50)
         except:
             pass
     print('CPM is {}'.format(mincount))
+    
+    data = [current_time,cpm_current]
+    writer.writerow(data)
     mincount = 0
